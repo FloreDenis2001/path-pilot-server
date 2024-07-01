0d08801575bf96f3159c28717b0b73604d580d0d
FloreDenis2001 <floredenis907@yahoo.com>
2024-07-01 22:16:50 +0300

    up

--- src/main/java/com/mycode/pathpilotserver/driver/services/DriverCommandServiceImpl.java
+++ src/main/java/com/mycode/pathpilotserver/driver/services/DriverCommandServiceImpl.java
@@ -28,6 +28,8 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
+import static com.mycode.pathpilotserver.city.utils.Utils.readCitiesFromJsonFile;
+
 
 @Service
 @Transactional
@@ -36,13 +38,11 @@ public class DriverCommandServiceImpl implements DriverCommandService {
     private final DriverRepo driverRepo;
     private final UserRepo userRepo;
     private final CompanyRepo companyRepo;
-    private final ObjectMapper objectMapper ;
 
     public DriverCommandServiceImpl(DriverRepo driverRepo, UserRepo userRepo, CompanyRepo companyRepo, ObjectMapper objectMapper) {
         this.driverRepo = driverRepo;
         this.userRepo = userRepo;
         this.companyRepo = companyRepo;
-        this.objectMapper = objectMapper;
     }
 
     @Override
@@ -106,10 +106,7 @@ public class DriverCommandServiceImpl implements DriverCommandService {
                 .population_proper(city.getPopulation_proper())
                 .build();
     }
-    private  List<City> readCitiesFromJsonFile() throws IOException {
-        File jsonFile = new File("C:\\Users\\denis\\OneDrive\\Desktop\\LUCRARE LICENTA\\path-pilot-server\\src\\main\\java\\com\\mycode\\pathpilotserver\\resource\\ro.json");
-        return objectMapper.readValue(jsonFile, new TypeReference<List<City>>() {});
-    }
+
 
     private City getCityByName(String cityName, List<City> cities) {
         return cities.stream()
