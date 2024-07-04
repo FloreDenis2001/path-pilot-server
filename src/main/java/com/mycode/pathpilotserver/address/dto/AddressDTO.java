40d45e31d38800c84657b4fd277f33e0948d002a
FloreDenis2001 <floredenis907@yahoo.com>
2024-07-04 17:24:06 +0300

    update

--- src/main/java/com/mycode/pathpilotserver/address/dto/AddressDTO.java
+++ src/main/java/com/mycode/pathpilotserver/address/dto/AddressDTO.java
@@ -5,6 +5,6 @@ import com.mycode.pathpilotserver.address.models.Address;
 public record AddressDTO(String city, String streetNumber, String street, String country, String postalCode) {
 
     public static AddressDTO from(Address address) {
-        return new AddressDTO(address.getCity(), address.getStreetNumber(), address.getStreet(), address.getCountry(), address.getPostalCode());
+        return new AddressDTO(address.getCityDetails().getCity(), address.getStreetNumber(), address.getStreet(), address.getCityDetails().getCountry(), address.getPostalCode());
     }
 }
